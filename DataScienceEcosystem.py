6458fe0b1d09ec158b58a93efa14cab79efef028
ranjan21-12 <141116963+ranjan21-12@users.noreply.github.com>
2023-08-01 22:50:43 +0530

    Add files via upload
    Practice file

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# * Popular data sciences languages are :- Python, sql, R, Julia etc.,
+# * Data science categories are :- Data management, Data integration & transformation, Data vizualisation, Model building, Model deployment and model monitoring and assesment
+# * Roles in data science :- Business analyst, Data base engineer, Data analyst, Data engineer, Data scientist, Software engineer etc.,
+# * Types of machine learning :- Supervised learning, unsupervised learning and reinforcement learning
+
+# Some of the popular languages that Data Scientists use are:
+# 1) PYTHON
+# 2) R
+# 3) SQL
+# 4) JULIA
+# 5) PHP
+
+# Some of the commonly used libraries used by Data Scientists include:
+# * Pandas
+# * Numpy
+# * Tensorflow
+# * Keras
+# * Scikit
+
+# |Data Science Tools|
+# |-----------------|
+# |jupyter|
+# |Apache zepplin|
+# |R studio|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[2]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# ## Author
+# 
+# Manoranjan Ganesh
+
+# In[ ]:
+
+
+
+
